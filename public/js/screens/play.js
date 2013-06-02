6495c09f73ea0f4cc1221f9e5491b5f9829391d0
Jason Oster <jay@kodewerx.org>
2013-06-02 00:13:54 -0700

    Add "Restart" button.

--- public/js/screens/play.js
+++ public/js/screens/play.js
@@ -1,6 +1,6 @@
 game.Moves = me.HUD_Item.extend({
     "init" : function (x, y, value) {
-        this.font = new me.Font("courier new", 50, "#ddd", "center");
+        this.font = new me.Font("Courier New", 50, "#ddd", "center");
         this.font.textBaseline = "middle";
         this.parent(x, y, value);
     },
@@ -10,11 +10,44 @@ game.Moves = me.HUD_Item.extend({
     }
 });
 
+game.Restart = me.Renderable.extend({
+    "init" : function (x, y) {
+        this.parent(new me.Vector2d(x, y), 128, 64);
+
+        this.font = new me.Font("Courier New", 25, "#ddd", "center");
+        this.font.textBaseline = "middle";
+
+        me.input.registerPointerEvent("mousedown", this, function (e) {
+            game.playscreen.loadPuzzle(game.playscreen.puzzle);
+        });
+    },
+
+    "destroy" : function () {
+        me.input.releasePointerEvent("mousedown", this);
+    },
+
+    "update" : function () {
+        return true;
+    },
+
+    "draw" : function (context) {
+        context.fillStyle = "#444";
+        context.fillRect(this.pos.x, this.pos.y, this.width, this.height);
+        this.font.draw(
+            context,
+            "Restart",
+            this.pos.x + this.width / 2,
+            this.pos.y + this.height / 2
+        );
+    }
+});
+
 game.PlayScreen = me.ScreenObject.extend({
     "onResetEvent" : function () {
         me.game.addHUD(320, 0, 160, C.HEIGHT);
         me.game.HUD.addItem("moves", new game.Moves(80, C.HEIGHT / 2, 0));
         me.game.add(new me.ColorLayer("background", "#333", 1));
+        me.game.add(new game.Restart(320 + 16, 320 - 96), 2);
 
         game.lights = [];
 
@@ -32,6 +65,10 @@ game.PlayScreen = me.ScreenObject.extend({
         this.loadPuzzle(this.puzzle);
     },
 
+    "onDestroyEvent" : function () {
+        me.game.disableHUD();
+    },
+
     "nextPuzzle" : function () {
         this.puzzle++;
         if (this.puzzle >= this.puzzles.length) {
@@ -39,7 +76,6 @@ game.PlayScreen = me.ScreenObject.extend({
                 me.state.GAMEOVER,
                 me.game.HUD.getItemValue("moves")
             );
-            me.game.disableHUD();
         }
         else {
             this.loadPuzzle(this.puzzle);
